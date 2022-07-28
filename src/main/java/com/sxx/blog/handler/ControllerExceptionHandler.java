07f36cde9180a674ee6c222ac5003bd41e505e3f
Aaron <Aaron@163.com>
2022-07-28 18:18:14 +0800

    sxx-blog-2022-07-28

---
+++ src/main/java/com/sxx/blog/handler/ControllerExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.sxx.blog.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.HttpServletBean;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+//    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(HttpServletRequest request,Exception e) throws Exception {
+        logger.error("Requst URL : {}, Exception : {}",request.getRequestURI(),e.getMessage());
+
+
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("url",request.getRequestURI());
+        mv.addObject("exception",e);
+        mv.setViewName("error/error");
+        return mv;
+    }
+
+}
